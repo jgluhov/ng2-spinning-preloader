a2d89fdaf32d7c5b11a4b0062d020ca3b39ff0ad
JGluhov <jgluhov@gmail.com>
2016-12-13 17:13:21 +0300

    rename spinner to spinning.

--- src/ng-spinning-preloader.constants.ts
+++ src/ng-spinning-preloader.constants.ts
@@ -1,5 +1,5 @@
 
-const CONTAINER_CLASS_NAME: string = 'spinner-preloader-container';
+const CONTAINER_CLASS_NAME: string = 'spinning-preloader-container';
 const COMPLETE_CLASS_NAME: string = 'complete';
 const CONTAINER_QUERY: string = `.${CONTAINER_CLASS_NAME}`;
 
